5f7af28c46fe1a0e5ee62f3ed82f5f53f218d9cc
matizaidan <leandromatiaszaidan@gmail.com>
2024-03-23 17:06:03 -0300

    Added view to create/edit blog post inside the site

--- blog/urls.py
+++ blog/urls.py
@@ -4,4 +4,6 @@ from . import views # (.) -> representa el directorio actual, importa todas las 
 urlpatterns = [
     path('', views.post_list, name='post_list'),
     path('post/<int:pk>/', views.post_detail, name='post_detail'),
+    path('post/new', views.post_new, name='post_new'),
+    path('post/<int:pk>/edit/', views.post_edit, name='post_edit'),
 ]
