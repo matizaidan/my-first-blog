4ed94226f418b8e43749e3b33b52cf425cc2c506
matizaidan <leandromatiaszaidan@gmail.com>
2024-09-08 14:59:49 -0300

    Agragando stylos.

--- mysite/settings.py
+++ mysite/settings.py
@@ -113,6 +113,7 @@ USE_TZ = True
 
 STATIC_URL = 'static/'
 STATIC_ROOT = BASE_DIR / 'staticfiles'  # Usualmente 'staticfiles' es la convención para la colección de archivos estáticos.
+STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
